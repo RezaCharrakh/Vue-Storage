baa6d2740bdbe15bc02b39058311b27a806830dd
RezaCharrakh <116076795+RezaCharrakh@users.noreply.github.com>
2024-09-17 17:48:04 +0330

    updating Hero section in login

--- nuxt-project/pages/login/index.vue
+++ nuxt-project/pages/login/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="flex">
-    <div class="flex">
-      <div>Left side 1</div>
-      <div>Left side 2</div>
+  <div class="flex justify-center items-center h-screen gap-52 relative">
+    <div class="flex relative">
+      <div class="flex">
+        <div class="mt-10 prose relative z-10 p-10">
+          <h1>
+            Securely store and access<br />
+            your files <br />anytime, anywhere!
+          </h1>
+          <div class="grad w-96 h-96 absolute -bottom-20 -right-7 -z-10"></div>
+          <div class="grad_org w-96 h-96 absolute -top-32 -left-32 -z-10"></div>
+        </div>
+        <div><img class="w-[30rem]" src="/assets/19 Internet of Things (IoT).png" alt="" /></div>
+      </div>
     </div>
-    <div class="flex flex-col items-center w-96 gap-5">
+    <div class="flex flex-col items-center w-96 gap-5 relative z-10 mr-52">
       <div class="w-[60%] mb-7"></div>
       <el-input type="email" v-model="email" placeholder="Email" />
       <el-input
@@ -14,7 +23,7 @@
         placeholder="Password"
       />
       <div class="relative w-full h-8">
-        <span class="text-gray-300 absolute right-0">Recover Password !?</span>
+        <span class="text-gray-300 absolute right-0">Recover Password ?</span>
       </div>
       <el-button @click="login" type="primary" class="w-full">Login</el-button>
       <el-divider>Continue with</el-divider>
@@ -42,6 +51,7 @@
         ></el-button>
       </div>
     </div>
+    <!-- Background Gradient -->
   </div>
 </template>
 
@@ -52,4 +62,23 @@ const email = ref("");
 const password = ref("");
 </script>
 
-<style scoped></style>
+<style scoped>
+.grad {
+  background: rgb(75, 104, 250);
+  background: radial-gradient(
+    circle 800px at center,
+    rgba(75, 104, 250, 0.3) 0%,
+    rgba(255, 255, 255, 0) 20%
+  );
+  z-index: -1; /* Make sure the gradient is behind the content */
+}
+
+.grad_org {
+  background: rgb(250, 204, 75);
+  background: radial-gradient(
+    circle 600px at center,
+    rgba(250, 204, 75, 0.17690826330532217) 0%,
+    rgba(255, 255, 255, 0) 30%
+  );
+}
+</style>
